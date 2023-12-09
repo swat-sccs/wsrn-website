173b77c1effc9943dbe628f32847c7956a6cbba2
Damian <37555910+DCRepublic@users.noreply.github.com>
2023-12-08 22:24:03 -0500

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import ThemeRegistry from './ThemeRegistry';
 import Header from './components/header.js';
 import Player from './components/player.js';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'WSRN Homepage',
